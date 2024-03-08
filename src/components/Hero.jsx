a314c9064bddb0c4fb37dfededf36c48e4ca3bd0
Florian SIMUNEK <florian.simunek@gmail.com>
2024-03-08 02:07:04 +0100

    ✨ Add Hero #2 section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,10 @@
 import ButtonLink from './ButtonLink';
+import profilePicture from '@assets/pp.webp';
 
 function Hero() {
   return (
-    <Container>
-      <Top>
+    <Container className="flex flex-col gap-2">
+      <Top className="flex flex-col gap-2">
         <div className="bg-primary text-black p-4 rounded-2xl w-full h-fit flex flex-col justify-between items-start gap-36">
           <h1 className="text-4xl font-extrabold uppercase">
             Prêt à faire de votre vision une réalité ?
@@ -36,15 +37,32 @@ function Hero() {
             </Bottom>
           </div>
         </div>
+        <div className="bg-white rounded-2xl overflow-hidden relative w-full min-h-96">
+          <img
+            src={profilePicture}
+            alt="Photo de profil"
+            className="absolute bottom-0 left-0 right-0 z-50"
+          />
+          <div className="w-full h-full flex flex-col py-4">
+            <TextBanner />
+            <TextBanner bordered />
+            <TextBanner />
+            <TextBanner bordered />
+            <TextBanner />
+            <TextBanner bordered />
+            <TextBanner />
+            <TextBanner bordered />
+          </div>
+        </div>
       </Top>
-      <Bottom></Bottom>
+      <Bottom className="flex flex-col gap-2"></Bottom>
     </Container>
   );
 }
 
-function Container({ children, ...props }) {
+function Container({ children, className, ...props }) {
   return (
-    <main className="w-svw h-svh p-2" {...props}>
+    <main className={`${className} w-svw h-fit p-2`} {...props}>
       {children}
     </main>
   );
@@ -56,6 +74,38 @@ function Top({ children, ...props }) {
 
 function Bottom({ children, ...props }) {
   return <div {...props}>{children}</div>;
+}
+
+function TextBanner({ bordered }) {
+  return (
+    <div className="text-wrapper text-5xl font-extrabold max-w-full overflow-hidden">
+      <div className="text-banner overflow-hidden inline-block whitespace-nowrap">
+        <p
+          className={`uppercase inline-block ${bordered ? 'text-primary' : ''}`}
+        >
+          Florian SIMUNEK
+        </p>
+        &nbsp;
+        <p
+          className={`uppercase inline-block ${bordered ? 'text-primary' : ''}`}
+        >
+          Florian SIMUNEK
+        </p>
+        &nbsp;
+        <p
+          className={`uppercase inline-block ${bordered ? 'text-primary' : ''}`}
+        >
+          Florian SIMUNEK
+        </p>
+        &nbsp;
+        <p
+          className={`uppercase inline-block ${bordered ? 'text-primary' : ''}`}
+        >
+          Florian SIMUNEK
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
