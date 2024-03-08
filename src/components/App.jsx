56bfd8ddd4354a2de27a0ced71f7f29e719e73c8
Florian SIMUNEK <florian.simunek@gmail.com>
2024-03-08 02:48:57 +0100

    ✨ Add Services sections

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,17 @@
 import Hero from './Hero';
+import Section from './Section';
 
 function App() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Section title="Créez votre site web" />
+      <Section title="Créez votre application React" primary />
+      <Section title="Intégration HTML & CSS" />
+      <Section title="Améliorez, réparez ou optimisez votre site web" primary />
+      <Section title="Créez votre site web par vous-même" />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Section.jsx
@@ -0,0 +1,41 @@
+import ButtonLink from './ButtonLink';
+
+function Section({ title, primary }) {
+  return (
+    <div
+      className={`${
+        primary ? 'bg-primary' : 'bg-white'
+      } w-full h-svh rounded-4xl p-8 flex flex-col justify-between items-center`}
+    >
+      <h2 className="uppercase font-extrabold text-5xl">{title}</h2>
+      <div className="bottom w-full flex justify-between items-center">
+        <div className="left">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={3}
+            stroke="currentColor"
+            className="w-12 h-12 rotate-90"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+            />
+          </svg>
+        </div>
+        <div className="right flex flex-col gap-2 justify-center items-end">
+          <ButtonLink className={`${primary ? '!bg-primary' : ''} uppercase`}>
+            Demander un devis
+          </ButtonLink>
+          <ButtonLink className={`${primary ? '!bg-primary' : ''} uppercase`}>
+            En savoir plus
+          </ButtonLink>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Section;
