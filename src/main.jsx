b95864d55a28e47412951114bed1eb73dfb93d37
Florian SIMUNEK <florian.simunek@gmail.com>
2024-03-11 19:20:06 +0100

    ➕ Add Lenis scroll

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,17 @@ import App from '@components/App.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import Lenis from '@studio-freight/lenis';
 import './main.css';
+
+/* LENIS */
+const lenis = new Lenis();
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+/* /LENIS */
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
