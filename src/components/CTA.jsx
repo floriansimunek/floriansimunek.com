5e2488ab40bfd43532b9ef119fb86046649cb4cc
Florian SIMUNEK <florian.simunek@gmail.com>
2024-03-12 15:01:09 +0100

    📱 Fix Hero tablet responsive

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -4,10 +4,10 @@ function CTA({ className, dark = false }) {
       href="https://floriansimunek.trafft.com/"
       target="_blank"
       rel="noreferrer"
-      className={`group relative ${className} text-black text-2xl md:text-4xl 2xl:text-5xl uppercase font-extrabold bg-primary rounded-2xl flex justify-between lg:justify-around items-center gap-2 px-8 py-12 transition-all overflow-hidden`}
+      className={`group relative ${className} flex items-center justify-between gap-2 overflow-hidden rounded-2xl bg-primary px-8 py-12 text-2xl font-extrabold uppercase text-black transition-all md:text-4xl lg:justify-around 2xl:text-5xl`}
     >
       <span
-        className={`flex flex-col text-start lg:flex-row z-50 ${
+        className={`z-50 flex flex-col text-start lg:flex-row ${
           dark ? 'group-hover:text-primary' : 'group-hover:text-black'
         }`}
       >
@@ -18,7 +18,7 @@ function CTA({ className, dark = false }) {
         fill="none"
         viewBox="0 0 24 24"
         strokeWidth={3}
-        className={`w-12 h-12 md:w-16 md:h-16 group-hover:translate-x-4 stroke-black ${
+        className={`h-12 w-12 stroke-black group-hover:translate-x-4 md:h-16 md:w-16 ${
           dark ? 'group-hover:stroke-primary' : 'group-hover:stroke-black'
         } z-50 transition-all`}
       >
@@ -31,7 +31,7 @@ function CTA({ className, dark = false }) {
       <span
         className={`${
           dark ? 'bg-black' : 'bg-white'
-        } absolute top-2/4 left-0 right-0 -translate-y-2/4 h-0 group-hover:h-full transition-all`}
+        } absolute left-0 right-0 top-2/4 h-0 -translate-y-2/4 transition-all group-hover:h-full`}
       ></span>
     </a>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,12 @@ import CTA from './CTA';
 function Hero() {
   return (
     <Container className="flex flex-col gap-2 lg:gap-4" id="hero">
-      <Top className="flex flex-col gap-2 lg:gap-4 md:flex-row lg:h-3/4">
-        <div className="bg-primary text-black p-4 lg:p-8 rounded-2xl w-full h-fit flex flex-col justify-between items-start gap-64 sm:gap-32 lg:gap-0 lg:w-4/6 lg:h-full">
-          <h1 className="text-6xl lg:text-8xl font-extrabold uppercase">
+      <Top className="flex flex-col gap-2 md:flex-row lg:h-3/4 lg:gap-4">
+        <div className="flex h-fit w-full flex-col items-start justify-between gap-64 rounded-2xl bg-primary p-4 text-black sm:gap-32 lg:h-full lg:w-4/6 lg:p-8 xl:gap-0">
+          <h1 className="text-5xl font-extrabold uppercase md:text-6xl lg:text-7xl xl:text-8xl">
             Prêt à faire de votre vision une réalité ?
           </h1>
-          <div className="flex flex-col gap-2 lg:gap-4 w-full lg:flex-row">
+          <div className="flex w-full flex-col gap-2 lg:gap-4 xl:flex-row">
             <Top>
               <ButtonLink
                 className="!w-full text-center"
@@ -23,7 +23,7 @@ function Hero() {
             </Top>
             <Bottom className="flex gap-2 lg:gap-4">
               <ButtonLink
-                className="w-full sm:w-fit text-center"
+                className="w-full text-center xl:w-fit"
                 to="https://www.linkedin.com/in/simunek-florian/"
                 target="_blank"
                 rel="noreferrer"
@@ -31,7 +31,7 @@ function Hero() {
                 LinkedIn
               </ButtonLink>
               <ButtonLink
-                className="w-full sm:w-fit text-center"
+                className="w-full text-center xl:w-fit"
                 to="https://bento.me/florian-simunek"
                 target="_blank"
                 rel="noreferrer"
@@ -41,41 +41,41 @@ function Hero() {
             </Bottom>
           </div>
         </div>
-        <div className="bg-white rounded-2xl overflow-hidden relative w-full min-h-96 sm:min-h-0 lg:w-2/6">
+        <div className="relative min-h-96 w-full overflow-hidden rounded-2xl bg-white sm:min-h-0 lg:w-2/6">
           <img
             src={profilePicture}
             alt="Photo de profil"
             className="absolute bottom-0 left-0 right-0 z-50"
           />
-          <div className="w-full h-full flex flex-col justify-between items-center py-4">
+          <div className="flex h-full w-full flex-col items-center justify-between py-4">
             <TextBanner />
-            <TextBanner bordered />
+            <TextBanner primary />
             <TextBanner />
-            <TextBanner bordered />
+            <TextBanner primary />
             <TextBanner />
-            <TextBanner bordered />
+            <TextBanner primary />
             <TextBanner />
-            <TextBanner bordered />
+            <TextBanner primary />
           </div>
         </div>
       </Top>
-      <Bottom className="flex flex-col gap-2 lg:gap-4 lg:flex-row lg:h-1/4">
-        <div className="stats flex gap-2 lg:gap-4 lg:w-full">
+      <Bottom className="flex flex-col gap-2 lg:h-1/4 lg:gap-4 xl:flex-row">
+        <div className="stats flex gap-2 lg:w-full lg:gap-4">
           <Stat>
             +140
-            <span className="font-bold text-xl md:text-2xl 2xl:text-3xl">
+            <span className="text-xl font-bold md:text-2xl 2xl:text-3xl">
               commandes
             </span>
           </Stat>
           <Stat>
             100%
-            <span className="font-bold text-xl md:text-2xl 2xl:text-3xl">
+            <span className="text-xl font-bold md:text-2xl 2xl:text-3xl">
               avis positifs
             </span>
           </Stat>
           <Stat className="hidden lg:flex">
             100%
-            <span className="font-bold text-xl md:text-2xl 2xl:text-3xl">
+            <span className="text-xl font-bold md:text-2xl 2xl:text-3xl">
               complétion
             </span>
           </Stat>
@@ -89,7 +89,7 @@ function Hero() {
 function Container({ children, className, ...props }) {
   return (
     <main
-      className={`${className} w-full h-fit lg:h-svh p-2 lg:p-4`}
+      className={`${className} h-fit w-full p-2 lg:p-4 xl:h-svh`}
       {...props}
     >
       {children}
@@ -105,30 +105,30 @@ function Bottom({ children, ...props }) {
   return <div {...props}>{children}</div>;
 }
 
-function TextBanner({ bordered }) {
+function TextBanner({ primary }) {
   return (
-    <div className="text-wrapper text-5xl font-extrabold max-w-full overflow-hidden">
-      <div className="text-banner overflow-hidden inline-block whitespace-nowrap">
+    <div className="text-wrapper max-w-full overflow-hidden text-5xl font-extrabold">
+      <div className="text-banner inline-block overflow-hidden whitespace-nowrap">
         <p
-          className={`uppercase inline-block ${bordered ? 'text-primary' : ''}`}
+          className={`inline-block uppercase ${primary ? 'text-primary' : ''}`}
         >
           Florian SIMUNEK
         </p>
         &nbsp;
         <p
-          className={`uppercase inline-block ${bordered ? 'text-primary' : ''}`}
+          className={`inline-block uppercase ${primary ? 'text-primary' : ''}`}
         >
           Florian SIMUNEK
         </p>
         &nbsp;
         <p
-          className={`uppercase inline-block ${bordered ? 'text-primary' : ''}`}
+          className={`inline-block uppercase ${primary ? 'text-primary' : ''}`}
         >
           Florian SIMUNEK
         </p>
         &nbsp;
         <p
-          className={`uppercase inline-block ${bordered ? 'text-primary' : ''}`}
+          className={`inline-block uppercase ${primary ? 'text-primary' : ''}`}
         >
           Florian SIMUNEK
         </p>
@@ -140,7 +140,7 @@ function TextBanner({ bordered }) {
 function Stat({ className, children }) {
   return (
     <h3
-      className={`${className} bg-white text-black rounded-2xl w-full min-h-32 flex flex-col justify-center items-center uppercase font-extrabold text-2xl md:text-4xl 2xl:text-5xl`}
+      className={`${className} flex min-h-32 w-full flex-col items-center justify-center rounded-2xl bg-white text-2xl font-extrabold uppercase text-black md:text-4xl 2xl:text-5xl`}
     >
       {children}
     </h3>
